991d760607541d121d3f8710b00628adfb8d5a8f
Mike Minutillo <michael.minutillo@gmail.com>
2017-03-21 20:52:19 +0800

    Report metric data to Metrics.NET

---
+++ src/NServiceBus.Monitoring/ProcessingTime/TraceReport.cs
@@ -0,0 +1,13 @@
+﻿namespace NServiceBus.Monitoring.ProcessingTime
+{
+    using System.Diagnostics;
+    using Metrics.Reporters;
+
+    class TraceReport : HumanReadableReport
+    {
+        protected override void WriteLine(string line, params string[] args)
+        {
+            Trace.WriteLine(string.Format(line, args));
+        }
+    }
+}
---
+++ src/SampleEndpoint/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SampleEndpoint
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync("Sample.Endpoint").GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string endpointName)
+        {
+            Console.Title = endpointName;
+
+            var endpointConfig = new EndpointConfiguration(endpointName);
+            endpointConfig.SendFailedMessagesTo("error");
+            endpointConfig.UsePersistence<InMemoryPersistence>();
+
+            var endpoint = await Endpoint.Start(endpointConfig);
+
+            Console.WriteLine($"{endpointName} started. Press [ESC] to close");
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                await endpoint.SendLocal(new SomeCommand());
+                await Task.Delay(100);
+            }
+
+            Console.WriteLine($"{endpointName} shutting down");
+
+            await endpoint.Stop();
+
+            Console.WriteLine($"{endpointName} stopped");
+        }
+    }
+
+    class SomeCommand : ICommand { }
+
+    class SomeCommandHandler : IHandleMessages<SomeCommand>
+    {
+        public Task Handle(SomeCommand message, IMessageHandlerContext context) => Task.CompletedTask;
+    }
+}
