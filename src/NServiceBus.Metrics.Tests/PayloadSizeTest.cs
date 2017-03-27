79b01fc7b34366c1f9e9cc2611aa6430a52a421f
SzymonPobiega <szymon.pobiega@gmail.com>
2017-03-27 11:34:22 +0200

    Added a test proving we can fir 500 metrics in a single message.

---
+++ src/NServiceBus.Metrics.Tests/PayloadSizeTest.cs
@@ -0,0 +1,37 @@
+﻿namespace NServiceBus.Metrics.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using global::Metrics;
+    using global::Metrics.Core;
+    using global::Metrics.Json;
+    using global::Metrics.MetricData;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PayloadSizeTest
+    {
+        const int MaxBodySize = 32 * 1024;
+
+        [Test]
+        public void It_can_fit_500_gauge_values_in_half_of_lowest_transport_message_limit()
+        {
+            var gauges = Enumerable.Range(0, 500).Select(i => new GaugeValueSource("Gauge " + i, new FunctionGauge(() => 666), Unit.Calls, MetricTags.None));
+            var data = new MetricsData("ContextName", DateTime.UtcNow, 
+                new EnvironmentEntry[0], 
+                gauges, 
+                new CounterValueSource[0], 
+                new MeterValueSource[0], 
+                new HistogramValueSource[0], 
+                new TimerValueSource[0], 
+                new MetricsData[0]);
+
+            var serialized = JsonBuilderV2.BuildJson(data);
+
+            var body = Encoding.UTF8.GetBytes(serialized);
+
+            Assert.IsTrue(body.Length < MaxBodySize);
+        }
+    }
+}
