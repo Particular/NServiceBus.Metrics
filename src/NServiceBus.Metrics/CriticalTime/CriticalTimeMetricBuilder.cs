e333b5e0c4689385422103b492a811dc0276c506
Mike Minutillo <michael.minutillo@gmail.com>
2017-03-28 12:18:42 +0800

    Add Critical Time

---
+++ src/NServiceBus.Metrics/CriticalTime/CriticalTimeMetricBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Metrics;
+using NServiceBus;
+using NServiceBus.Features;
+
+class CriticalTimeMetricBuilder : IMetricBuilder
+{
+    public void WireUp(FeatureConfigurationContext featureConfigurationContext, MetricsContext metricsContext, Unit messagesUnit)
+    {
+        var criticalTimeTimer = metricsContext.Timer("Critical Time", messagesUnit);
+
+        featureConfigurationContext.Pipeline.OnReceivePipelineCompleted(e =>
+        {
+            DateTime timeSent;
+            if (e.TryGetTimeSent(out timeSent))
+            {
+                var endToEndTime = e.CompletedAt - timeSent;
+
+                criticalTimeTimer.Record((long)endToEndTime.TotalMilliseconds, TimeUnit.Milliseconds);
+            }
+
+            return Task.FromResult(0);
+        });
+    }
+}
