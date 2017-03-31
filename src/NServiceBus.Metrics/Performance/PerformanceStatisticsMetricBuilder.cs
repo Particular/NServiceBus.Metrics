b645248e4c664d89e1649e1c93267fc0ff100822
Mike Minutillo <michael.minutillo@gmail.com>
2017-03-31 15:42:37 +0800

    Rename meters to match Perf Counters

--- src/NServiceBus.Metrics/Performance/PerformanceStatisticsMetricBuilder.cs
+++ src/NServiceBus.Metrics/Performance/PerformanceStatisticsMetricBuilder.cs
@@ -5,9 +5,9 @@ class PerformanceStatisticsMetricBuilder : IMetricBuilder
 {
     public void WireUp(FeatureConfigurationContext featureConfigurationContext, MetricsContext metricsContext, Unit messagesUnit)
     {
-        var messagesPulledFromQueueMeter = metricsContext.Meter("Messages Pulled from Queue", messagesUnit);
-        var failureRateMeter = metricsContext.Meter("Message Processing Failures", messagesUnit);
-        var successRateMeter = metricsContext.Meter("Messages Successfully Processed", messagesUnit);
+        var messagesPulledFromQueueMeter = metricsContext.Meter("# of messages pulled from the input queue / sec", messagesUnit);
+        var failureRateMeter = metricsContext.Meter("# of message failures / sec", messagesUnit);
+        var successRateMeter = metricsContext.Meter("# of messages successfully processed / sec", messagesUnit);
 
         var performanceDiagnosticsBehavior = new ReceivePerformanceDiagnosticsBehavior(
             messagesPulledFromQueueMeter,
