fccbdce99cd4924f629cd9bd2198d9f8196242e3
Mike Minutillo <michael.minutillo@gmail.com>
2017-04-04 15:42:50 +0800

    Hide ServiceControl API Take 2 (#16)

--- src/NServiceBus.Metrics/MetricsOptions.cs
+++ src/NServiceBus.Metrics/MetricsOptions.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
     using global::Metrics;
     using global::Metrics.Reports;
     using Logging;
@@ -19,7 +18,7 @@
         /// </summary>
         /// <param name="serviceControlMetricsAddress">The transport address of the ServiceControl instance</param>
         /// <param name="interval">How frequently metric data is sent to ServiceControl</param>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Obsolete("Not for public use.")]
         public void SendMetricDataToServiceControl(string serviceControlMetricsAddress, TimeSpan interval)
         {
             Guard.AgainstNullAndEmpty(nameof(serviceControlMetricsAddress), serviceControlMetricsAddress);
