ea13b1ccc68c6f1be6d2486df2752588f8604d1c
Mike Minutillo <michael.minutillo@gmail.com>
2017-04-04 12:45:02 +0800

    Remove defaults

--- src/NServiceBus.Metrics/MetricsOptions.cs
+++ src/NServiceBus.Metrics/MetricsOptions.cs
@@ -18,15 +18,14 @@
         /// </summary>
         /// <param name="serviceControlMetricsAddress">The transport address of the ServiceControl instance</param>
         /// <param name="interval">How frequently metric data is sent to ServiceControl</param>
-        /// <remarks>If no interval is specified then the Default Interval is used</remarks>
-        public void SendMetricDataToServiceControl(string serviceControlMetricsAddress, TimeSpan? interval = null)
+        public void SendMetricDataToServiceControl(string serviceControlMetricsAddress, TimeSpan interval)
         {
             Guard.AgainstNullAndEmpty(nameof(serviceControlMetricsAddress), serviceControlMetricsAddress);
             Guard.AgainstNegativeAndZero(nameof(interval), interval);
 
             reportInstallers.Add((builder, config) => config.WithReport(
                 new NServiceBusMetricReport(builder.Build<IDispatchMessages>(), serviceControlMetricsAddress),
-                interval ?? defaultInterval
+                interval
             ));
         }
 
@@ -34,14 +33,13 @@
         /// Enables sending metric data to the trace log
         /// </summary>
         /// <param name="interval">How often metric data is sent to the trace log</param>
-        /// <remarks>If no interval is specified then the Default Interval is used</remarks>
-        public void EnableMetricTracing(TimeSpan? interval = null)
+        public void EnableMetricTracing(TimeSpan interval)
         {
             Guard.AgainstNegativeAndZero(nameof(interval), interval);
 
             reportInstallers.Add((builder, config) => config.WithReport(
                 new TraceReport(),
-                interval ?? defaultInterval
+                interval
             ));
         }
 
@@ -50,29 +48,14 @@
         /// </summary>
         /// <param name="interval">How often metric data is sent to the log</param>
         /// <param name="logLevel">Level at which log entries should be written. Default is DEBUG.</param>
-        /// <remarks>
-        /// If no interval is specified then the Default Interval is used.
-        /// Metrics data will be logged at the INFO log level
-        /// </remarks>
-        public void EnableLogTracing(TimeSpan? interval = null, LogLevel? logLevel = null)
+        public void EnableLogTracing(TimeSpan interval, LogLevel logLevel = LogLevel.Debug)
         {
             Guard.AgainstNegativeAndZero(nameof(interval), interval);
 
             reportInstallers.Add((builder, config) => config.WithReport(
-                new MetricsLogReport(logLevel ?? LogLevel.Debug),
-                interval ?? defaultInterval
+                new MetricsLogReport(logLevel),
+                interval
             ));
-        }
-
-        /// <summary>
-        /// Sets the default interval for reporting metric data
-        /// </summary>
-        /// <param name="interval">The new default interval</param>
-        public void SetDefaultInterval(TimeSpan interval)
-        {
-            Guard.AgainstNegativeAndZero(nameof(interval), interval);
-
-            defaultInterval = interval;
         }
 
         internal void SetUpReports(MetricsConfig config, IBuilder builder)
@@ -80,7 +63,6 @@
             config.WithReporting(reportsConfig => reportInstallers.ForEach(installer => installer(builder, reportsConfig)));
         }
 
-        TimeSpan defaultInterval = TimeSpan.FromSeconds(30);
         List<Action<IBuilder, MetricsReports>> reportInstallers = new List<Action<IBuilder, MetricsReports>>();
     }
 }
