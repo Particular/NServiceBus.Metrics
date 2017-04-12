cbce8e02872809a831b39ae24c138f8e008b0c97
danielmarbach <daniel.marbach@openplace.net>
2017-04-12 17:44:38 +0200

    Moved properties to derived

--- src/NServiceBus.Metrics/Core/MetricAttribute.cs
+++ src/NServiceBus.Metrics/Core/MetricAttribute.cs
@@ -10,37 +10,6 @@ namespace NServiceBus.Metrics
     abstract class MetricAttribute : Attribute
     {
         /// <summary>
-        /// Creates new metric attribute.
-        /// </summary>
-        protected MetricAttribute(string name, string unit, string description, string[] tags = null)
-        {
-            Name = name;
-            Unit = unit;
-            Description = description;
-            Tags = tags;
-        }
-
-        /// <summary>
-        /// Name of the meter
-        /// </summary>
-        public string Name { get; }
-
-        /// <summary>
-        /// Unit of measure
-        /// </summary>
-        public string Unit { get; }
-
-        /// <summary>
-        /// Description.
-        /// </summary>
-        public string Description { get; }
-
-        /// <summary>
-        /// Optional tags
-        /// </summary>
-        public string[] Tags { get; }
-
-        /// <summary>
         /// Defines the metric in a given context.
         /// </summary>
         public abstract object DefineMetric(MetricsContext context);
