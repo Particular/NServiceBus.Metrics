06cafde566ac36481f23696728b830b30d80fe8e
Simon Cropp <simon.cropp@gmail.com>
2017-06-15 10:22:20 +1000

    Create Snapshotting.cs

--- src/NServiceBus.Metrics/Core/Snapshotting.cs
+++ src/NServiceBus.Metrics/Core/Snapshotting.cs
@@ -4,7 +4,8 @@
     using System.Linq;
     using global::Metrics.MetricData;
 
-    static class MetricsDataExtentions {
+    static class MetricsDataExtentions 
+    {
 
         public static MetricsData ResetMetrics(this MetricsData data)
         {
