a01f72b3a9d11fdc91ab764368b1563fab11d8ed
Szymon Kulec <scooletz@gmail.com>
2017-06-23 18:10:53 +0200

    Writer extracted from reporter allowing custom writers to be used

---
+++ src/NServiceBus.Metrics/RawData/LongValueWriter.cs
@@ -0,0 +1,55 @@
+﻿namespace NServiceBus.Metrics.RawData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    static class LongValueWriter
+    {
+        const long Version = 1;
+
+        // WIRE FORMAT, Version: 1
+
+        // 0                   1                   2                   3
+        // 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //|     Version   | Min date time | date1 |    value 1    | date2 |  
+        //+---------------+---------------+-------------------------------+
+        //|     value 2   | date3 |   value  3    | date4 |     value 4   |
+        //+---------------+---------------+-------------------------------+
+
+        public static void Write(BinaryWriter outputWriter, ArraySegment<RingBuffer.Entry> chunk)
+        {
+            var array = chunk.Array;
+            var offset = chunk.Offset;
+            var count = chunk.Count;
+
+            Array.Sort(array, offset, count, EntryTickComparer.Instance);
+
+            var minDate = array[offset].Ticks;
+
+            outputWriter.Write(Version);
+            outputWriter.Write(minDate);
+
+            for (var i = 0; i < count; i++)
+            {
+                // int allows to write ticks of 7minutes, as reporter runs much more frequent, this can be int
+                var date = (int)(array[offset + i].Ticks - minDate);
+                outputWriter.Write(date);
+                outputWriter.Write(array[offset + i].Value);
+            }
+        }
+
+        class EntryTickComparer : IComparer<RingBuffer.Entry>
+        {
+            public static readonly EntryTickComparer Instance = new EntryTickComparer();
+
+            EntryTickComparer() { }
+
+            public int Compare(RingBuffer.Entry x, RingBuffer.Entry y)
+            {
+                return x.Ticks.CompareTo(y.Ticks);
+            }
+        }
+    }
+}
--- src/NServiceBus.Metrics/RawData/RawDataReporter.cs
+++ src/NServiceBus.Metrics/RawData/RawDataReporter.cs
@@ -11,32 +11,34 @@
     using Support;
     using Transport;
 
+    delegate void WriteOutput(ArraySegment<RingBuffer.Entry> entries, BinaryWriter outputWriter);
+
     class RawDataReporter
     {
-        public const long Version = 1;
-
         readonly RingBuffer buffer;
+        readonly Action<ArraySegment<RingBuffer.Entry>> outputWriter;
         readonly IDispatchMessages dispatcher;
-        UnicastAddressTag destination;
-        TransportTransaction transportTransaction = new TransportTransaction();
-        Dictionary<string, string> headers = new Dictionary<string, string>();
-        BinaryWriter writer;
-        MemoryStream memoryStream;
-        CancellationTokenSource cancellationTokenSource;
+        readonly UnicastAddressTag destination;
+        readonly TransportTransaction transportTransaction = new TransportTransaction();
+        readonly Dictionary<string, string> headers = new Dictionary<string, string>();
+        readonly BinaryWriter writer;
+        readonly MemoryStream memoryStream;
+        readonly CancellationTokenSource cancellationTokenSource;
         Task reporter;
         static readonly TimeSpan delayTime = TimeSpan.FromSeconds(1);
 
-        public RawDataReporter(IDispatchMessages dispatcher, string destination, HostInformation hostInformation, RingBuffer buffer)
+        public RawDataReporter(IDispatchMessages dispatcher, string destination, HostInformation hostInformation, RingBuffer buffer, string messageTypeName, string endpointName,
+            WriteOutput outputWriter)
         {
             this.buffer = buffer;
+            this.outputWriter = entries=>outputWriter(entries,writer);
             this.dispatcher = dispatcher;
             this.destination = new UnicastAddressTag(destination);
 
             headers[Headers.OriginatingMachine] = RuntimeEnvironment.MachineName;
             headers[Headers.OriginatingHostId] = hostInformation.HostId.ToString("N");
-            headers[Headers.EnclosedMessageTypes] = "NServiceBus.Metrics.RawData"; // without assembly name to allow ducktyping
-            headers[Headers.ContentType] = ContentTypes.Json;
-            headers[Headers.OriginatingEndpoint] = "SOMETHING";
+            headers[Headers.EnclosedMessageTypes] = "NServiceBus.Metrics." + messageTypeName; 
+            headers[Headers.OriginatingEndpoint] = endpointName;
 
             memoryStream = new MemoryStream();
             writer = new BinaryWriter(memoryStream);
@@ -52,7 +54,7 @@
                     // clear stream first
                     memoryStream.SetLength(0);
 
-                    var consumed = buffer.Consume(Consume);
+                    var consumed = buffer.Consume(outputWriter);
 
                     if (consumed == 0)
                     {
@@ -75,51 +77,6 @@
         {
             cancellationTokenSource.Cancel();
             return reporter;
-        }
-
-        void Consume(ArraySegment<RingBuffer.Entry> chunk)
-        {
-            var array = chunk.Array;
-            var offset = chunk.Offset;
-            var count = chunk.Count;
-
-            Array.Sort(array, offset, count, EntryTickComparer.Instance);
-
-            var minDate = array[offset].Ticks;
-
-            // WIRE FORMAT, Version: 1
-
-            // 0                   1                   2                   3
-            // 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-            //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-            //|     Version   | Min date time | date1 |    value 1    | date2 |  
-            //+---------------+---------------+-------------------------------+
-            //|     value 2   | date3 |   value  3    | date4 |     value 4   |
-            //+---------------+---------------+-------------------------------+
-            
-
-            writer.Write(Version);
-            writer.Write(minDate);
-
-            for (var i = 0; i < count; i++)
-            {
-                // int allows to write ticks of 7minutes, as reporter runs much more frequent, this can be int
-                var date = (int)(array[offset + i].Ticks - minDate);
-                writer.Write(date);
-                writer.Write(array[offset + i].Value);
-            }
-        }
-
-        class EntryTickComparer : IComparer<RingBuffer.Entry>
-        {
-            public static readonly EntryTickComparer Instance = new EntryTickComparer();
-
-            EntryTickComparer() { }
-
-            public int Compare(RingBuffer.Entry x, RingBuffer.Entry y)
-            {
-                return x.Ticks.CompareTo(y.Ticks);
-            }
         }
     }
 }
