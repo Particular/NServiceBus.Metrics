8c288c760270c789274f9ef4ce7f579e5777c12b
Szymon Kulec <scooletz@gmail.com>
2017-09-27 13:13:43 +0200

    No more custom reporting.

--- src/NServiceBus.Metrics/MetricsOptions.cs
+++ src/NServiceBus.Metrics/MetricsOptions.cs
@@ -1,65 +1,12 @@
 ﻿namespace NServiceBus
 {
     using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using global::Metrics;
-    using global::Metrics.Reports;
-    using Logging;
 
     /// <summary>
     /// Provides configuration options for Metrics feature
     /// </summary>
     public class MetricsOptions
     {
-        /// <summary>
-        /// Enables sending metric data to the trace log
-        /// </summary>
-        /// <param name="interval">How often metric data is sent to the trace log</param>
-        [ObsoleteEx(RemoveInVersion = "3.0", TreatAsErrorFromVersion = "3.0", Message = "Use RegisterObservers instead to attach to probes.")]
-        public void EnableMetricTracing(TimeSpan interval)
-        {
-            Guard.AgainstNegativeAndZero(nameof(interval), interval);
-
-            legacyReportInstallers.Add(config => config.WithReport(
-                new TraceReport(),
-                interval
-            ));
-        }
-
-        /// <summary>
-        /// Enables sending metric data to the NServiceBus log
-        /// </summary>
-        /// <param name="interval">How often metric data is sent to the log</param>
-        /// <param name="logLevel">Level at which log entries should be written. Default is DEBUG.</param>
-        [ObsoleteEx(RemoveInVersion = "3.0", TreatAsErrorFromVersion = "3.0", Message = "Use RegisterObservers instead to attach to probes.")]
-        public void EnableLogTracing(TimeSpan interval, LogLevel logLevel = LogLevel.Debug)
-        {
-            Guard.AgainstNegativeAndZero(nameof(interval), interval);
-
-            legacyReportInstallers.Add(config => config.WithReport(
-                new MetricsLogReport(logLevel),
-                interval
-            ));
-        }
-
-        /// <summary>
-        /// Enables custom report, allowing to consume data by any func.
-        /// </summary>
-        /// <param name="func">A function that will be called with a raw JSON.</param>
-        /// <param name="interval">How often metric data is sent to the log</param>
-        [ObsoleteEx(RemoveInVersion = "3.0", TreatAsErrorFromVersion = "3.0", Message = "Use RegisterObservers instead to attach to probes.")]
-        public void EnableCustomReport(Func<string, Task> func, TimeSpan interval)
-        {
-            Guard.AgainstNull(nameof(func), func);
-            Guard.AgainstNegativeAndZero(nameof(interval), interval);
-
-            legacyReportInstallers.Add(config => config.WithReport(
-                new CustomReport(func),
-                interval
-            ));
-        }
-
         /// <summary>
         /// Enables registering observers to available probes.
         /// </summary>
@@ -76,13 +23,6 @@
             registerObservers(probeContext);
         }
 
-        internal void SetUpLegacyReports(MetricsConfig config)
-        {
-            config.WithReporting(reportsConfig => legacyReportInstallers.ForEach(installer => installer(reportsConfig)));
-        }
-
         Action<ProbeContext> registerObservers = c => {};
-
-        List<Action<MetricsReports>> legacyReportInstallers = new List<Action<MetricsReports>>();
     }
 }
