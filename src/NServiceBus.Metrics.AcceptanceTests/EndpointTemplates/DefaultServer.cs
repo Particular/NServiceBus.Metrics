bbeb57d5b7bee18f2d7dc91576766ccd5e0b1f37
Szymon Kulec <scooletz@gmail.com>
2017-10-06 11:02:24 +0200

    NSB.Metrics for NServiceBus 7.0 (#65)

--- src/NServiceBus.Metrics.AcceptanceTests/EndpointTemplates/DefaultServer.cs
+++ src/NServiceBus.Metrics.AcceptanceTests/EndpointTemplates/DefaultServer.cs
@@ -2,12 +2,12 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Threading.Tasks;
     using AcceptanceTesting.Customization;
     using AcceptanceTesting.Support;
-    using Configuration.AdvanceExtensibility;
+    using Configuration.AdvancedExtensibility;
     using Features;
-    using Config.ConfigurationSource;
 
     public class DefaultServer : IEndpointSetupTemplate
     {
@@ -21,12 +21,8 @@
             this.typesToInclude = typesToInclude;
         }
 
-#pragma warning disable CS0618
-        public async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration, IConfigurationSource configSource, Action<EndpointConfiguration> configurationBuilderCustomization)
-#pragma warning restore CS0618
+        public Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration, Action<EndpointConfiguration> configurationBuilderCustomization)
         {
-            var settings = runDescriptor.Settings;
-
             var types = endpointConfiguration.GetTypesScopedByTestClass();
 
             typesToInclude.AddRange(types);
@@ -34,28 +30,26 @@
             var configuration = new EndpointConfiguration(endpointConfiguration.EndpointName);
 
             configuration.TypesToIncludeInScan(typesToInclude);
-            configuration.CustomConfigurationSource(configSource);
             configuration.EnableInstallers();
 
             configuration.DisableFeature<TimeoutManager>();
+
+            configuration.UsePersistence<InMemoryPersistence>();
+            var storageDir = Path.Combine(NServiceBusAcceptanceTest.StorageRootDir, NUnit.Framework.TestContext.CurrentContext.Test.ID);
+            configuration.UseTransport<LearningTransport>()
+                .StorageDirectory(storageDir);
 
             var recoverability = configuration.Recoverability();
             recoverability.Delayed(delayed => delayed.NumberOfRetries(0));
             recoverability.Immediate(immediate => immediate.NumberOfRetries(0));
             configuration.SendFailedMessagesTo("error");
 
-            await configuration.DefineTransport(settings, endpointConfiguration)
-                .ConfigureAwait(false);
-
             configuration.RegisterComponentsAndInheritanceHierarchy(runDescriptor);
-
-            await configuration.DefinePersistence(settings, endpointConfiguration)
-                .ConfigureAwait(false);
 
             configuration.GetSettings().SetDefault("ScaleOut.UseSingleBrokerQueue", true);
             configurationBuilderCustomization(configuration);
 
-            return configuration;
+            return Task.FromResult(configuration);
         }
 
         List<Type> typesToInclude;
